8542355c4482fa19677b69a6c92fe1e7929cf993
BakaRena <68040760+BakaRena@users.noreply.github.com>
2020-07-27 12:49:12 +0900

    Add files via upload

---
+++ Stalin joke.py
@@ -0,0 +1,42 @@
+rows = 5
+print("There are " + str(rows) + " rows of party members in attendance " + "at the Congress Hall during Stalin's speech.")
+
+print ("Suddenly, someone sneezes.")
+print ("Stalin asks, 'Who sneezed?'")
+print ("No one answers.")
+print ("Stalin says: 'Row number " + str(rows) + ", on your feet! Shoot them!'")
+
+rows -= 1
+
+print ("Suddenly, someone sneezes.")
+print ("Stalin asks, 'Who sneezed?'")
+print ("No one answers.")
+print ("Stalin says: 'Row number " + str(rows) + ", on your feet! Shoot them!'")
+
+rows -= 1
+
+print ("Suddenly, someone sneezes.")
+print ("Stalin asks, 'Who sneezed?'")
+print ("No one answers.")
+print ("Stalin says: 'Row number " + str(rows) + ", on your feet! Shoot them!'")
+
+rows -= 1
+
+print ("Suddenly, someone sneezes.")
+print ("Stalin asks, 'Who sneezed?'")
+print ("No one answers.")
+print ("Stalin says: 'Row number " + str(rows) + ", on your feet! Shoot them!'")
+
+rows -= 1
+
+print ("Suddenly, someone sneezes.")
+print ("Stalin asks, 'Who sneezed?'")
+print ("No one answers.")
+print ("Stalin says: 'Row number " + str(rows) + ", on your feet! Shoot them!'")
+
+rows -= 1
+
+print ("At last a sobbing cry resounds" + " in the Congress Hall:")
+print ("'It was me! Me!'")
+print ("Stalin says:")
+print ("'Bless you, Comrade!' and resumes his speech")
